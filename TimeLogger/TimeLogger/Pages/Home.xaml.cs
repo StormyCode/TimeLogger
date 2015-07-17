64f3607eede378c4ca61b22aa69fd174f3520641
treolandix <treolandix@gmail.com>
2015-07-18 00:58:01 +0200

    txtbox_end Eventhändler hinzu

--- TimeLogger/TimeLogger/Pages/Home.xaml.cs
+++ TimeLogger/TimeLogger/Pages/Home.xaml.cs
@@ -24,5 +24,10 @@ namespace TimeLogger.Pages
         {
             InitializeComponent();
         }
+
+        private void txtbox_end_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
     }
 }
