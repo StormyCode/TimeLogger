fa90fad3fafe2d0ad7c45c3748c89451d2931fe3
StormyCode <julian.heep@gmail.com>
2015-07-17 23:18:30 +0200

    TimeLoggerController UpdateLogFile Methode hinzugefügt

--- TimeLogger/TimeLogger/TimeLoggerController.cs
+++ TimeLogger/TimeLogger/TimeLoggerController.cs
@@ -20,5 +20,27 @@ namespace TimeLogger
         /// Liste aller LogItems
         /// </summary>
         public List<LogItem> LogList { get; private set; }
+        /// <summary>
+        /// Überschreibt das LogFile mit allen Einträgen der LogList
+        /// </summary>
+        public void UpdateLogFile()
+        {
+            //Überprüft, ob Pfad und Datei gesetzt sind
+            if (this.ExportDirectory != null && this.ExportFileName != null)
+            {
+                //Überprüft, ob Pfad exisitert und legt diesen gegebenfalls an
+                if (!System.IO.Directory.Exists(this.ExportDirectory))
+                    System.IO.Directory.CreateDirectory(this.ExportDirectory);
+                //Legt einen StreamWriter fürs LogFile an (Modus = überschreiben)
+                using (System.IO.StreamWriter sw = new System.IO.StreamWriter(String.Format("{0}/{1}", this.ExportDirectory, this.ExportFileName), false))
+                {
+                    //Schreibt jedes LogItem ins LogFile
+                    foreach (LogItem item in this.LogList)
+                    {
+                        sw.WriteLine(item.ToString());
+                    }
+                }
+            }
+        }
     }
 }
