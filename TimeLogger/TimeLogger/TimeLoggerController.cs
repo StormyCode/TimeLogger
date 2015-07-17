cfb3f77a0328513d7f277758cf83bc755f04713b
StormyCode <julian.heep@gmail.com>
2015-07-17 23:05:00 +0200

    TimeLogger Variablen hinzugefügt

--- TimeLogger/TimeLogger/TimeLoggerController.cs
+++ TimeLogger/TimeLogger/TimeLoggerController.cs
@@ -8,5 +8,17 @@ namespace TimeLogger
 {
     class TimeLoggerController
     {
+        /// <summary>
+        /// Pfad, an dem Daten gespeichert werden
+        /// </summary>
+        public string ExportDirectory { get; private set; }
+        /// <summary>
+        /// Name der Export-Datei
+        /// </summary>
+        public string ExportFileName { get; private set; }
+        /// <summary>
+        /// Liste aller LogItems
+        /// </summary>
+        public List<LogItem> LogList { get; private set; }
     }
 }
